e5220850aaa051c39aa11cbcb2f1ba526fb493d5
Dionne Gonçalves <dionnegoncalves@gmail.com>
2024-05-20 17:39:34 -0300

    Teste

---
+++ ObrigadoAVotar.java
@@ -0,0 +1,12 @@
+public class ObrigadoAVotar {
+
+    public static void main(String[] args) {
+      int numero = 10;
+      numero++; // aqui número passa a valer 11
+      numero--; // aqui número passa a valer 10
+      System.out.println(numero);
+    }
+  }
+
+
+
